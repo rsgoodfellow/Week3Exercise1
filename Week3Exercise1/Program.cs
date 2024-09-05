4a24183d3a2962e97396f997a642f0a37a0d19ff
rsgoodfellow <rsgoodfellow@amarillocollege.com>
2024-09-04 22:04:27 -0500

    added comments

--- Week3Exercise1/Program.cs
+++ Week3Exercise1/Program.cs
@@ -8,97 +8,97 @@ namespace Week3Exercise1
 {
     internal class Program
     {
-        public static double ConvertToUSD(double amount, string fromCurrency)
+        public static double ConvertToUSD(double amount, string fromCurrency) //method to convert the inputed amount to USD currency
         {
-            double exchangeRate = 0;
+            double exchangeRate = 0; //declares and initializes exchangeRate variable
 
-            if (fromCurrency == "EUR")
+            if (fromCurrency == "EUR") //if condition if fromCurrency is equal to EUR
             {
-                exchangeRate = 1.1083;
+                exchangeRate = 1.1083; //sets the exchange rate variable
             }
-            else if (fromCurrency == "JPY")
+            else if (fromCurrency == "JPY") //else if condition if fromCurrency is equal to JPY
             {
-                exchangeRate = 0.0070;
+                exchangeRate = 0.0070; //sets the exchange rate variable
             }
-            else
+            else //else condition if fromCurrency does not equal any valid options
             {
-                Console.WriteLine("Please enter a valid currency.");
-                return 0;
+                Console.WriteLine("Please enter a valid currency."); //displays text informing user the input was invalid
+                return 0; //returns a 0 for a default output
             }
 
-            return exchangeRate * amount;
+            return exchangeRate * amount; //calculates the estimated conversion of currency
         }
 
-        public static double ConvertToEUR(double amount, string fromCurrency)
+        public static double ConvertToEUR(double amount, string fromCurrency) //method to convert the inputed amount to EUR currency
         {
-            double exchangeRate = 0;
+            double exchangeRate = 0; //declares and initializes exchangeRate variable
 
-            if (fromCurrency == "USD")
+            if (fromCurrency == "USD") //if condition if fromCurrency is equal to USD
             {
-                exchangeRate = 0.9022;
+                exchangeRate = 0.9022; //sets the exchange rate variable
             }
-            else if (fromCurrency == "JPY")
+            else if (fromCurrency == "JPY") //else if condition if fromCurrency is equal to JPY
             {
-                exchangeRate = 0.0063;
+                exchangeRate = 0.0063; //sets the exchange rate variable
             }
-            else
+            else //else condition if fromCurrency does not equal any valid options
             {
-                Console.WriteLine("Please enter a valid currency.");
-                return 0;
+                Console.WriteLine("Please enter a valid currency."); //displays text informing user the input was invalid
+                return 0; //returns a 0 for a default output
             }
 
-            return exchangeRate * amount;
+            return exchangeRate * amount; //calculates the estimated conversion of currency
         }
 
-        public static double ConvertToJPY(double amount, string fromCurrency)
+        public static double ConvertToJPY(double amount, string fromCurrency) //method to convert the inputed amount to JPY currency
         {
-            double exchangeRate = 0;
+            double exchangeRate = 0; //declares and initializes exchangeRate variable
 
-            if (fromCurrency == "USD")
+            if (fromCurrency == "USD") //if condition if fromCurrency is equal to USD
             {
-                exchangeRate = 143.655;
+                exchangeRate = 143.655; //sets the exchange rate variable
             }
-            else if (fromCurrency == "EUR")
+            else if (fromCurrency == "EUR") //else if condition if fromCurrency is equal to EUR
             {
-                exchangeRate = 159.23;
+                exchangeRate = 159.23; //sets the exchange rate variable
             }
-            else
+            else //else condition if fromCurrency does not equal any valid options
             {
-                Console.WriteLine("Please enter a valid currency.");
-                return 0;
+                Console.WriteLine("Please enter a valid currency."); //displays text informing user the input was invalid
+                return 0; //returns a 0 for a default output
             }
 
-            return exchangeRate * amount;
+            return exchangeRate * amount; //calculates the estimated conversion of currency
         }
 
-        static void Main(string[] args)
+        static void Main(string[] args) //main method
         {
-            Console.Write("Please enter the amount you would like to convert: ");
-            double amountInput = Convert.ToDouble(Console.ReadLine());
-            Console.Write("Please enter the currency you are converting from (USD, EUR, JPY): ");
-            string currentCurrency = Console.ReadLine();
+            Console.Write("Please enter the amount you would like to convert: "); //prompts user to input a double number
+            double amountInput = Convert.ToDouble(Console.ReadLine()); //converts user input into a double variable
+            Console.Write("Please enter the currency you are converting from (USD, EUR, JPY): "); //prompts the user for what currency they are using
+            string currentCurrency = Console.ReadLine(); //converts user input to currentCurrency variable
 
-            if (currentCurrency == "USD")
+            if (currentCurrency == "USD") //if condition if current currency is USD
             {
-                Console.WriteLine(amountInput + " in USD would be " + ConvertToEUR(amountInput, currentCurrency) + " in EUR.");
-                Console.WriteLine(amountInput + " in USD would be " + ConvertToJPY(amountInput, currentCurrency) + " in JPY.");
+                Console.WriteLine(amountInput + " in USD would be " + ConvertToEUR(amountInput, currentCurrency) + " in EUR."); //displays converted currency
+                Console.WriteLine(amountInput + " in USD would be " + ConvertToJPY(amountInput, currentCurrency) + " in JPY."); //displays converted currency
             }
-            else if (currentCurrency == "EUR")
+            else if (currentCurrency == "EUR") //if condition if current currency is EUR
             {
-                Console.WriteLine(amountInput + " in EUR would be " + ConvertToUSD(amountInput, currentCurrency) + " in USD.");
-                Console.WriteLine(amountInput + " in EUR would be " + ConvertToJPY(amountInput, currentCurrency) + " in JPY.");
+                Console.WriteLine(amountInput + " in EUR would be " + ConvertToUSD(amountInput, currentCurrency) + " in USD."); //displays converted currency
+                Console.WriteLine(amountInput + " in EUR would be " + ConvertToJPY(amountInput, currentCurrency) + " in JPY."); //displays converted currency
             }
-            else if (currentCurrency == "JPY")
+            else if (currentCurrency == "JPY") //if condition if current currency is JPY
             {
-                Console.WriteLine(amountInput + " in JPY would be " + ConvertToEUR(amountInput, currentCurrency) + " in EUR.");
-                Console.WriteLine(amountInput + " in JPY would be " + ConvertToUSD(amountInput, currentCurrency) + " in USD.");
+                Console.WriteLine(amountInput + " in JPY would be " + ConvertToEUR(amountInput, currentCurrency) + " in EUR."); //displays converted currency
+                Console.WriteLine(amountInput + " in JPY would be " + ConvertToUSD(amountInput, currentCurrency) + " in USD."); //displays converted currency
             }
-            else
+            else//else condition if currency does not equal any valid options
             {
-                Console.WriteLine("Enter a valid currency");
+                Console.WriteLine("Enter a valid currency"); //displays text informing user to input a valid currency
             }
 
-            Console.ReadKey();
+            Console.ReadKey(); //allows user to read program
         }
     }
 }
